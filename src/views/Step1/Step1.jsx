a5ee42c50c4341e492ea1fd23f9f2a25eb81cdb8
MouaadLaouar <laouarmouaad@gmail.com>
2023-03-08 08:31:33 +0100

    first commit

--- src/views/Step1/Step1.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Step1() {
-  return (
-    <div>Step1</div>
-  )
-}
--- src/views/Step2/Step2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Step2() {
-  return (
-    <div>Step2</div>
-  )
-}
